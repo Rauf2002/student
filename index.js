971113025de344f7d9d21f42c928362809e3b7fc
RaufSa@code.edu.az <RaufSa@code.edu.az>
2019-03-29 12:26:07 +0400

    student

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict'
+
 function Structure(ad, soyad, yas, email, id) {
     this.name = ad;
     this.surname = soyad;
@@ -14,19 +15,36 @@ let wrapper = document.querySelectorAll('#body')[0]
 function submitPerson() {
     let name = document.forms['register']['name'].value;
     let surname = document.forms['register']['surname'].value;
-    let age = document.forms['register']['birthday'].value;
+    let birthday = document.forms['register']['birthday'].value;
     let email = document.forms['register']['email'].value;
     let id = arrayPeople.length + 1;
+    let age = calculateAge(birthday);
     let Person = new Structure(name, surname, age, email, id);
     arrayPeople.push(Person);
     displayNames()
+    console.log(arrayPeople)
+
 }
 
+// Cedvelde adi gosteririk :
 function displayNames() {
     let list = '<table>'
     for (let i = 0; i < arrayPeople.length; i++) {
-        list += '<tr><th scope="row">' + arrayPeople[i].id + '</th>' + '<td>' + arrayPeople[i].name + '</td>' + '<td>' + arrayPeople[i].surname + '</td>' + '<td>' + '</td>' + '<td>' + arrayPeople[i].email + '</td></tr>'
+        list += '<tr><th scope="row">' + arrayPeople[i].id + '</th>' + '<td>' + arrayPeople[i].name + '</td>' + '<td>' + arrayPeople[i].surname + '</td>' + '<td>' + arrayPeople[i].age + '</td>' + '<td>' + arrayPeople[i].email + '</td></tr>'
     }
     list += '</table>';
     wrapper.innerHTML = list;
+}
+
+// Yasi tapiriq :
+
+function calculateAge(birthday) {
+    let today = new Date();
+    let birthDate = new Date(birthday);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || m === 0 && today.getDate() < birthDate.getDate()) {
+        age--;
+    }
+    return age;
 }
